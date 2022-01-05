3bcb0774124678331c04b3e04517e15a6f78754f
magimbijr <charleatanasi@gmail.com>
2022-01-05 11:37:48 +0300

    Theming

--- app/src/main/java/com/tana/safaritour/MainActivity.kt
+++ app/src/main/java/com/tana/safaritour/MainActivity.kt
@@ -3,10 +3,8 @@ package com.tana.safaritour
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,22 +20,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SafariTourTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/tana/safaritour/ui/theme/Color.kt
+++ app/src/main/java/com/tana/safaritour/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.tana.safaritour.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val AliceBlue = Color(0xFFE5EBF0)
+val EerieBlack = Color(0xFF212121)
+val BottleGreen = Color(0xFF2D6A4F)
+val OpaqueDark = Color(0x28FFFFFF)
+val Cultured = Color(0xFFF2F2F2)
