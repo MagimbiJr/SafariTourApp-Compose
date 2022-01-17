14ec9455a18b0d65ae0c15222eac3222ff2aa021
magimbijr <charleatanasi@gmail.com>
2022-01-17 12:11:24 +0300

    Add data model

---
+++ app/src/main/java/com/tana/safaritour/bottom_nav/home/data/Place.kt
@@ -0,0 +1,13 @@
+package com.tana.safaritour.bottom_nav.home.data
+
+data class Place(
+    val name: String,
+    val location: String,
+    val type: String,
+    val isFavorite: Boolean,
+    val rates: Int,
+    val description: String,
+    val image: String
+) {
+    constructor() : this("", "", "", false, 0,"", "")
+}
